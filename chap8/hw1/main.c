49c73053acf171d3b672a92cccad1b74c75ab22c
swon0219-png <swon0219@cu.ac.kr>
2025-11-04 11:17:49 +0900

    hw1

--- chap8/hw1/main.c
+++ chap8/hw1/main.c
@@ -39,48 +39,35 @@ int main(int argc, char *argv[]) {
     }
 
     for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "-e") == 0) {
-            if (i + 1 < argc && argv[i + 1][0] != '-') {
+        if (strncmp(argv[i], "-e", 2) == 0) {
+            if (strlen(argv[i]) > 2) {
+                char *env_name = argv[i] + 2;
+                print_env(env_name);
+            }
+            else if (i + 1 < argc && argv[i + 1][0] != '-') {
                 print_env(argv[i + 1]);
                 i++;
             } else {
                 print_env(NULL);
             }
-        } 
+        }
         else if (strcmp(argv[i], "-u") == 0) {
-            uid_t real_uid = getuid();
-            uid_t effective_uid = geteuid();
-            struct passwd *real_pw = getpwuid(real_uid);
-            struct passwd *eff_pw  = getpwuid(effective_uid);
-
-            printf("My Realistic User ID : %d (%s)\n", 
-                   real_uid, real_pw ? real_pw->pw_name : "unknown");
-            printf("My Valid User ID : %d (%s)\n", 
-                   effective_uid, eff_pw ? eff_pw->pw_name : "unknown");
-        } 
+            printf("Real UID: %d, Effective UID: %d\n", getuid(), geteuid());
+        }
         else if (strcmp(argv[i], "-g") == 0) {
-            gid_t real_gid = getgid();
-            gid_t effective_gid = getegid();
-            struct group *real_gr = getgrgid(real_gid);
-            struct group *eff_gr  = getgrgid(effective_gid);
-
-            printf("My Realistic Group ID : %d (%s)\n",
-                   real_gid, real_gr ? real_gr->gr_name : "unknown");
-            printf("My Valid Group ID : %d (%s)\n",
-                   effective_gid, eff_gr ? eff_gr->gr_name : "unknown");
-        } 
+            printf("Real GID: %d, Effective GID: %d\n", getgid(), getegid());
+        }
         else if (strcmp(argv[i], "-i") == 0) {
-            printf("my process number : [%d]\n", getpid());
-        } 
+            printf("Process ID: %d\n", getpid());
+        }
         else if (strcmp(argv[i], "-p") == 0) {
-            printf("my parent's process number : [%d]\n", getppid());
-        } 
+            printf("Parent Process ID: %d\n", getppid());
+        }
         else if (strcmp(argv[i], "-h") == 0) {
             print_help(argv[0]);
-        } 
+        }
         else {
             fprintf(stderr, "Unknown option: %s\n", argv[i]);
-            print_help(argv[0]);
         }
     }
 
